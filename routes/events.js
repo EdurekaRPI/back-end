1db3abcc43d22806d0c17bb05885e52969954df7
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2025-02-11 16:55:50 -0500

    deleted node modules and .env

--- routes/events.js
+++ routes/events.js
@@ -5,9 +5,17 @@ const Event = require('../models/eventModelSuperset'); // Adjust path as needed
 // GET events that have posters
 router.get('/events-with-posters', async (req, res) => {
     try {
-        const eventsWithPosters = await Event.find(
-            { image: { $exists: true, $ne: null } } // Filter for events with a poster
-        ).select('title description startDateTime endDateTime location image'); // Return only necessary fields
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Normalize to start of the day
+
+        const weekLater = new Date();
+        weekLater.setDate(today.getDate() + 14); // Get date 14 days from today
+        weekLater.setHours(23, 59, 59, 999); // Set to end of the day
+
+        const eventsWithPosters = await Event.find({
+            image: { $exists: true, $ne: null }, // Filter for events with a poster
+            startDateTime: { $gte: today, $lte: weekLater } // Events within 7-day window
+        }).select('title description startDateTime endDateTime location image');
 
         res.json(eventsWithPosters);
     } catch (error) {
