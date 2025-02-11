e8624d0a095798a4789d2aac2909c29178c155eb
Nibroc6 <corbinl.lnibroc@gmail.com>
2025-02-11 15:19:40 -0500

    update model; create EventHub->Eureka translator

--- routes/api_eventhub.js
+++ routes/api_eventhub.js
@@ -4,10 +4,73 @@ var router = express.Router();
 const Event = require('../models/eventModelSuperset');
 
 
-function convertEventhubToER(input){
-	console.log("converter called");
-	return input;
+/*
+EventHub Model:
+{
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  creationTimestamp: { type: Date, default: Date.now },
+  poster: { type: String, required: true },
+  startDateTime: { type: Date, required: true },
+  endDateTime: { type: Date, required: true },
+  location: { type: String, required: true },
+  image: { type: String },
+  tags: [String],
+  club: { type: String, required: true },
+  rsvp: { type: String },
 }
+*/
+
+/*
+Our Model:
+{
+	eventID: { type: String, required: true },
+    title: { type: String, required: true },
+	description: { type: String, required: true },
+    typeOfEvent: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    creationTimestamp: { type: Date, default: Date.now },
+    eventCreator: { type: String, required: true },
+    eventHost: { type: String, required: true },
+    attendees: { type: Array, default:[] },
+    startDateTime: { type: Date, required: true },
+    endDateTime: { type: Date, required: true },
+    location: { type: String, required: true },
+    classroomID: {type: String},
+    image: { type: String },
+    tags: {type: [String] },
+    club: { type: String, required: true },
+    rsvpMethod: { type: String },
+    externalRef: { type: mongoose.Schema.ObjectId },
+}
+*/
+
+
+function convertEventhubToER(input){
+	//console.log(input.titlle);
+	console.log("converter called");
+	return {
+		hubID: input._id.$oid,
+		title: input.title,
+		typeOfEvent: "EventhubAutogen",
+		description: input.description,
+		likes: input.likes,
+		creationTimestamp: input.creationTimestamp.$date,
+		eventCreator: input.poster,
+		eventHost: input.club,
+		startDateTime: input.startDateTime.$date,
+		endDateTime: input.endDateTime.$date,
+		location: input.location,
+		image: input.image,
+		tags: input.tags,
+		rsvp: input.rsvpMethod,
+		
+		club: input.club,
+	};
+}
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,7 +80,6 @@ router.get('/', function(req, res, next) {
     } catch (err) {
         res.status(500).json({ error: 'Error :(', error_details: err });
     }
-	
 });
 
 router.post('/newEvent', async (req, res) => {
