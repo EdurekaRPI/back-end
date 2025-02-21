6cdd1fda14f5672ec9b46e8e2ea948a265829b50
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2025-02-21 17:18:13 -0500

    I still can't figure out how to run the db correctly to pull events. Changed the port which it runs on for my local convenience. I think my route works, but again, I am struggling to test it

--- routes/index.js
+++ routes/index.js
@@ -272,7 +272,7 @@ app.delete('/events/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
