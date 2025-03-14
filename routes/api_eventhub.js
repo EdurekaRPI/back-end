3d9e52ebd98ffa34d31ac3704ecf6af40f46f5a2
Nibroc6 <corbinl.lnibroc@gmail.com>
2025-03-14 16:17:40 -0400

    Update readme; clean up some code; add more secure api key salt

--- routes/api_eventhub.js
+++ routes/api_eventhub.js
@@ -11,6 +11,9 @@ const currentAuthLocation = "EventHub";
 const {
   scrypt,
 } = require('node:crypto');
+require('dotenv').config({ path: './.env' });
+const APIkeySalt = process.env.APIkeySalt;
+
 
 
 
@@ -59,13 +62,13 @@ Our Model:
 const auth = async (req, res, next) => {
 	// Define the logic for authentication (e.g., check for a key, verify credentials)
 	const apiAuthKey = req.get('Api-Key');
-
+	console.log(APIkeySalt);
 	//check if the user gave us an API key in the header
 	if (apiAuthKey) {
 		//look for the given key in the DB
-		await scrypt(apiAuthKey, 'salt', 64, async (err, keyHash) => {
+		await scrypt(apiAuthKey, APIkeySalt, 64, async (err, keyHash) => {
 			keyHash = keyHash.toString('hex');
-			//console.log("API hash: ",keyHash);
+			console.log("API hash: ",keyHash);
 			if (err) throw err;
 			apiUser = await ApiKeys.findOne({key: keyHash});
 			//if we found them, check their perms
