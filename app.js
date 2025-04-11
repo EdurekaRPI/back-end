2f1164b7a455d9612a40a9b90aee33a7bc01cbe2
Nibroc6 <corbinl.lnibroc@gmail.com>
2025-04-11 17:33:12 -0400

    change cors header accept options

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   
   // Allow specific headers
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', '*');
   
   // Allow specific HTTP methods
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
